c401ed3c426b0e8640e92720587d7b607ac6cdf6
Lukasz Gryzbon <lukasz.gryzbon@news.co.uk>
2019-09-01 14:32:41 +0100

    Unit tests for argument validation added

---
+++ src/test/java/com/test/PricingApplicationTest.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class PricingApplicationTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailIfNoArgumentsPassed() {
+
+        // WHEN
+        PricingApplication.main(new String[]{});
+    }
+
+    @Test
+    public void shouldFailIfNoArgumentsPassedWitMeaningfulMessage() {
+
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Argument missing. Usage: $ gradle run --args=\"{}\"");
+
+        // WHEN
+        PricingApplication.main(new String[]{});
+    }
+}
