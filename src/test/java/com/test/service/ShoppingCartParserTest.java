57d74bebe483fad902e6676f628815740fdbcb1c
Lukasz Gryzbon <lukasz.gryzbon@news.co.uk>
2019-09-01 16:41:46 +0100

    ShoppingCartParserTest updated

--- src/test/java/com/test/service/ShoppingCartParserTest.java
+++ src/test/java/com/test/service/ShoppingCartParserTest.java
@@ -1,7 +1,9 @@
 package com.test.service;
 
+import static com.test.model.ProductEnum.APPLE;
 import static com.test.model.ProductEnum.BREAD;
 import static com.test.model.ProductEnum.MILK;
+import static com.test.model.ProductEnum.SOUP;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -17,7 +19,7 @@ public class ShoppingCartParserTest {
     private ShoppingCartParser shoppingCartParser = new ShoppingCartParser();
 
     @Test
-    public void shouldJsonWithSingleItem() throws IOException {
+    public void shouldParseJsonWithSingleItem() throws IOException {
         // GIVEN
         final String shoppingCartJson = "{\"milk\":1}";
 
@@ -31,17 +33,19 @@ public class ShoppingCartParserTest {
     }
 
     @Test
-    public void shouldJsonWithTwoItems() throws IOException {
+    public void shouldParseJsonWithMultipleItems() throws IOException {
         // GIVEN
-        final String shoppingCartJson = "{\"milk\":1, \"bread\":1}";
+        final String shoppingCartJson = "{\"soup\":1, \"bread\":2, \"milk\":3, \"apple\":4}";
 
         // WHEN
         Map<ProductEnum, Integer> shoppingCart = shoppingCartParser.parse(shoppingCartJson);
 
         // THEN
-        assertThat(shoppingCart.size(), is(2));
-        assertThat(shoppingCart.get(MILK), is(1));
-        assertThat(shoppingCart.get(BREAD), is(1));
+        assertThat(shoppingCart.size(), is(4));
+        assertThat(shoppingCart.get(SOUP), is(1));
+        assertThat(shoppingCart.get(BREAD), is(2));
+        assertThat(shoppingCart.get(MILK), is(3));
+        assertThat(shoppingCart.get(APPLE), is(4));
     }
 
 }
