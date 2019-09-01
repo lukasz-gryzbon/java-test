a466173e1e86e391be7ed145b5864f3507f85ed3
Lukasz Gryzbon <lukasz.gryzbon@news.co.uk>
2019-09-01 16:38:57 +0100

    ShoppingCartParser updated to handle multiple items

--- src/main/java/com/test/service/ShoppingCartParser.java
+++ src/main/java/com/test/service/ShoppingCartParser.java
@@ -1,16 +1,19 @@
 package com.test.service;
 
-import static com.test.model.ProductEnum.MILK;
-
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.test.model.ProductEnum;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ShoppingCartParser {
-    public Map<ProductEnum, Integer> parse(String shoppingCartJson) {
-        final Map<ProductEnum, Integer> shoppingCart = new HashMap<>();
-        shoppingCart.put(MILK, 1);
-        return shoppingCart;
+    private TypeReference<HashMap<ProductEnum, Integer>> typeRef = new TypeReference<HashMap<ProductEnum, Integer>>() {};
+
+    public Map<ProductEnum, Integer> parse(String shoppingCartJson) throws IOException {
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(shoppingCartJson.toUpperCase(), typeRef);
     }
 }
--- src/test/java/com/test/service/ShoppingCartParserTest.java
+++ src/test/java/com/test/service/ShoppingCartParserTest.java
@@ -1,12 +1,15 @@
 package com.test.service;
 
+import static com.test.model.ProductEnum.BREAD;
 import static com.test.model.ProductEnum.MILK;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import com.test.model.ProductEnum;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class ShoppingCartParserTest {
@@ -14,7 +17,7 @@ public class ShoppingCartParserTest {
     private ShoppingCartParser shoppingCartParser = new ShoppingCartParser();
 
     @Test
-    public void shouldJsonWithSingleItem() {
+    public void shouldJsonWithSingleItem() throws IOException {
         // GIVEN
         final String shoppingCartJson = "{\"milk\":1}";
 
@@ -25,6 +28,20 @@ public class ShoppingCartParserTest {
         assertThat(shoppingCart.size(), is(1));
         assertTrue(shoppingCart.containsKey(MILK));
         assertTrue(shoppingCart.containsValue(1));
+    }
+
+    @Test
+    public void shouldJsonWithTwoItems() throws IOException {
+        // GIVEN
+        final String shoppingCartJson = "{\"milk\":1, \"bread\":1}";
+
+        // WHEN
+        Map<ProductEnum, Integer> shoppingCart = shoppingCartParser.parse(shoppingCartJson);
+
+        // THEN
+        assertThat(shoppingCart.size(), is(2));
+        assertThat(shoppingCart.get(MILK), is(1));
+        assertThat(shoppingCart.get(BREAD), is(1));
     }
 
 }
