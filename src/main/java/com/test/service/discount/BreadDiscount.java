78f0ce6e59b30393fec7a998f141b1160813c8cb
Lukasz Gryzbon <lukasz.gryzbon@news.co.uk>
2019-09-01 18:15:24 +0100

    BreadDiscount logic implemented

--- src/main/java/com/test/service/discount/BreadDiscount.java
+++ src/main/java/com/test/service/discount/BreadDiscount.java
@@ -1,5 +1,8 @@
 package com.test.service.discount;
 
+import static com.test.model.ProductEnum.BREAD;
+import static com.test.model.ProductEnum.SOUP;
+
 import com.test.model.ProductEnum;
 
 import java.util.Map;
@@ -8,6 +11,20 @@ public class BreadDiscount implements Discount {
 
     @Override
     public double calculateDiscountValue(final Map<ProductEnum, Integer> shoppingCart) {
+        final int maxDiscountedLoaves = getDiscountedLoavesOfBread(shoppingCart);
+        if (maxDiscountedLoaves > 0 && shoppingCart.containsKey(BREAD)) {
+            final Integer loavesOfBread = shoppingCart.get(BREAD);
+            final int fullPriceLoaves = Math.max(loavesOfBread - maxDiscountedLoaves, 0);
+            final int discountedLoaves = loavesOfBread - fullPriceLoaves;
+            return discountedLoaves * BREAD.getPrice() / 2;
+        }
         return 0.0;
+    }
+
+    private int getDiscountedLoavesOfBread(Map<ProductEnum, Integer> shoppingCart) {
+        if (shoppingCart.containsKey(SOUP) && shoppingCart.get(SOUP) > 1) {
+            return shoppingCart.get(SOUP) / 2;
+        }
+        return 0;
     }
 }
